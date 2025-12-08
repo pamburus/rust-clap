1926fb0f1dafbaf47721ddc107ca1a39863b2890
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2025-12-08 22:24:59 +0100

    feat(mangen): Add verbatim option

--- clap_mangen/src/lib.rs
+++ clap_mangen/src/lib.rs
@@ -22,6 +22,7 @@ pub struct Man {
     date: String,
     source: String,
     manual: String,
+    verbatim: bool,
 }
 
 /// Build a [`Man`]
@@ -48,6 +49,7 @@ impl Man {
             date,
             source,
             manual,
+            verbatim: false,
         }
     }
 
@@ -94,6 +96,18 @@ impl Man {
     pub fn manual(mut self, manual: impl Into<String>) -> Self {
         self.manual = manual.into();
         self
+    }
+
+    /// Keep line breaks in arguments help
+    pub fn verbatim(mut self, keep: bool) -> Self {
+        self.verbatim = keep;
+        self
+    }
+
+    fn settings(&self) -> render::Parameters {
+        render::Parameters {
+            verbatim: self.verbatim,
+        }
     }
 }
 
@@ -265,7 +279,7 @@ impl Man {
 
         if !args.is_empty() {
             roff.control("SH", ["OPTIONS"]);
-            render::options(roff, &args);
+            render::options(roff, &args, &self.settings());
         }
 
         for heading in help_headings {
@@ -275,7 +289,7 @@ impl Man {
                 .partition(|&a| a.get_help_heading() == Some(heading));
 
             roff.control("SH", [heading.to_uppercase().as_str()]);
-            render::options(roff, &args);
+            render::options(roff, &args, &self.settings());
         }
     }
 
